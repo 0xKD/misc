60a9eeb1a195e477249a3477522246667946b5fb
Kedar <kedardeore@gmail.com>
2016-04-28 00:54:19 +0530

    Update fragment to load background image from 500px

--- java/android/learn/Weather/app/src/main/java/mixedquantum/blogspot/com/weather/ui/fragments/CurrentWeatherFragment.java
+++ java/android/learn/Weather/app/src/main/java/mixedquantum/blogspot/com/weather/ui/fragments/CurrentWeatherFragment.java
@@ -2,18 +2,33 @@ package mixedquantum.blogspot.com.weather.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.Locale;
+
+import jp.wasabeef.picasso.transformations.BlurTransformation;
 import mixedquantum.blogspot.com.weather.R;
 import mixedquantum.blogspot.com.weather.models.CurrentWeather;
+import mixedquantum.blogspot.com.weather.models.photos.SearchResult;
 import mixedquantum.blogspot.com.weather.ui.activities.BaseActivity;
 import mixedquantum.blogspot.com.weather.utils.Constants;
+import mixedquantum.blogspot.com.weather.utils.ServiceDelegate;
+import mixedquantum.blogspot.com.weather.utils.SpaceTime;
+import mixedquantum.blogspot.com.weather.utils.Temperature;
 
 public class CurrentWeatherFragment extends BaseFragment {
     private CurrentWeather currentWeather;
+    private ImageView imgBackground;
+    private TextView txtDescription;
 
     public static CurrentWeatherFragment newInstance(CurrentWeather weather) {
         CurrentWeatherFragment fragment = new CurrentWeatherFragment();
@@ -39,12 +54,44 @@ public class CurrentWeatherFragment extends BaseFragment {
         }
         if (currentWeather != null) {
             TextView txtLocation = (TextView) view.findViewById(R.id.location);
-            TextView txtDescription = (TextView) view.findViewById(R.id.description);
+            txtDescription = (TextView) view.findViewById(R.id.description);
             TextView txtTemperature = (TextView) view.findViewById(R.id.temperature);
             txtLocation.setText(currentWeather.getName());
             txtDescription.setText(currentWeather.getWeather().get(0).getDescription());  // add check!
-            txtTemperature.setText(currentWeather.getMain().getTemp().toString());  // formatting?
+            Double temp = Temperature.kelvinToCelsius(currentWeather.getMain().getTemp());
+            txtTemperature.setText(String.format(Locale.getDefault(), "%.1f°", temp));
+            imgBackground = (ImageView) view.findViewById(R.id.background_image);
+            getBackgroundImage();
         }
         return view;
+    }
+
+    private void getBackgroundImage() {
+        String term = String.format(Locale.getDefault(), "%s %s",
+                txtDescription.getText(), SpaceTime.getPreciseName());
+        ServiceDelegate.getInstance().getPhoto(term.trim());
+    }
+
+    private void getBackgroundImage(boolean precise) {
+        // TODO: Clean up this and above method, progressively lower term precision
+        if (!precise) {
+            ServiceDelegate.getInstance().getPhoto(txtDescription.getText().toString());
+        } else {
+            getBackgroundImage();
+        }
+    }
+
+    @Subscribe
+    public void onImageEvent(SearchResult searchResult) {
+        if (searchResult.getPhotos().size() == 1) {
+            Picasso.with(getContext())
+                .load(searchResult.getPhotos().get(0).getImageUrl())
+                .fit()
+                .centerCrop()
+                .transform(new BlurTransformation(getContext(), 72))
+                .into(imgBackground);
+        } else {
+            Log.e(TAG, "No photo found!");
+        }
     }
 }
