12e8d8d570013f92d2f338f1c5d7f0e057dc55b8
Kedar <kedardeore@gmail.com>
2016-04-30 18:09:44 +0530

    Load fragment only once (onCreate)

--- java/android/learn/Weather/app/src/main/java/mixedquantum/blogspot/com/weather/ui/activities/MainActivity.java
+++ java/android/learn/Weather/app/src/main/java/mixedquantum/blogspot/com/weather/ui/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package mixedquantum.blogspot.com.weather.ui.activities;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import mixedquantum.blogspot.com.weather.R;
 import mixedquantum.blogspot.com.weather.ui.fragments.CurrentWeatherFragment;
 
@@ -8,6 +11,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         CurrentWeatherFragment fragment = CurrentWeatherFragment.newInstance(1275339L);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
