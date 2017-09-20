39de935e0b1c5e5fed63689d42d7ebfbac290149
Kedar <kedardeore@gmail.com>
2017-09-20 10:22:56 +0530

    Beej's guide to C

---
+++ c/learn/bgc/src/intro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void) {
+	int num = 0, result = 0;
+	scanf("%d", &num);
+
+	for (int i = 1; i <= num; i++) {
+		result += i;
+	}
+
+	printf("%d\n", result);
+
+	return 0;
+}
